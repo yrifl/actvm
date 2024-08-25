0b78923fdf2f6e9e5cdc60c2f7e03b6f96c466f1
RebornEnder (backup) <enderop44rblx+bruh@gmail.com>
2024-08-26 00:36:59 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -5,5 +5,5 @@ Write-Host "[+] Downloading + Installing chrome rdp"
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 Write-Host "[+] Done."
 Write-Host "[+] Downloading + Installing chrome"
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/silent /install' -Verb RunAs -Wait; Remove-Item $P}
 Write-Host "[+] Done."
