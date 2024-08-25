702cff9b95a47f23e85b469dc0068d707dabe07a
RebornEnder (backup) <enderop44rblx+bruh@gmail.com>
2024-08-26 00:25:49 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,6 @@
+Write-Host "Setting network profile"
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Write-Host "Downloading chrome rdp"
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+Write-Host "Installing chrome"
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
